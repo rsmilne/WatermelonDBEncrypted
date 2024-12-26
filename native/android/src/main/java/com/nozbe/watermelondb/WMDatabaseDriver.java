49f45ad57e49bfbd201e28b11a99df6356a1927f
rsmilne <rodneysmilne@gmail.com>
2024-12-26 17:04:06 -0300

    FIx dependency

--- native/android/src/main/java/com/nozbe/watermelondb/WMDatabaseDriver.java
+++ native/android/src/main/java/com/nozbe/watermelondb/WMDatabaseDriver.java
@@ -1,15 +1,12 @@
 package com.nozbe.watermelondb;
 
 import android.content.Context;
-import android.database.Cursor;
+import net.sqlcipher.Cursor;
 import net.sqlcipher.database.SQLiteDatabase;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.WritableArray;
-import com.nozbe.watermelondb.utils.MigrationSet;
-import com.nozbe.watermelondb.utils.Pair;
-import com.nozbe.watermelondb.utils.Schema;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,51 +17,13 @@ import java.util.logging.Logger;
 public class WMDatabaseDriver {
     private final WMDatabase database;
     private final String dbName;
-
-    private final Logger log;
+    private final Logger logger;
     private final Map<String, List<String>> cachedRecords;
 
-    public WMDatabaseDriver(Context context, String dbName) {
-        this(context, dbName, false);
-    }
-
-    public WMDatabaseDriver(Context context, String dbName, int schemaVersion, boolean unsafeNativeReuse) {
-        this(context, dbName, unsafeNativeReuse);
-        SchemaCompatibility compatibility = isCompatible(schemaVersion);
-        if (compatibility instanceof SchemaCompatibility.NeedsSetup) {
-            throw new SchemaNeededError();
-        } else if (compatibility instanceof SchemaCompatibility.NeedsMigration) {
-            throw new MigrationNeededError(
-                    ((SchemaCompatibility.NeedsMigration) compatibility).fromVersion
-            );
-
-        }
-
-    }
-
-    public WMDatabaseDriver(Context context, String dbName, Schema schema, boolean unsafeNativeReuse) {
-        this(context, dbName, unsafeNativeReuse);
-        unsafeResetDatabase(schema);
-    }
-
-    public WMDatabaseDriver(Context context, String dbName, MigrationSet migrations, boolean unsafeNativeReuse) {
-        this(context, dbName, unsafeNativeReuse);
-        migrate(migrations);
-    }
-
-    public WMDatabaseDriver(Context context, String dbName, boolean unsafeNativeReuse) {
-        this.database = unsafeNativeReuse ? WMDatabase.getInstance(dbName, context,
-                SQLiteDatabase.CREATE_IF_NECESSARY |
-                        SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING) :
-                WMDatabase.buildDatabase(dbName, context,
-                        SQLiteDatabase.CREATE_IF_NECESSARY |
-                                SQLiteDatabase.ENABLE_WRITE_AHEAD_LOGGING);
+    public WMDatabaseDriver(String dbName, Context context, String encryptionKey) {
+        this.database = WMDatabase.getInstance(dbName, context, encryptionKey);
         this.dbName = dbName;
-        if (BuildConfig.DEBUG) {
-            this.log = Logger.getLogger("DB_Driver");
-        } else {
-            this.log = null;
-        }
+        this.logger = Logger.getLogger("WMDatabaseDriver");
         this.cachedRecords = new HashMap<>();
     }
 
@@ -72,128 +31,9 @@ public class WMDatabaseDriver {
         return database;
     }
 
-    public Object find(String table, String id) {
-        if (isCached(table, id)) {
-            return id;
-        }
+    public boolean find(String table, String id) {
         String[] args = new String[]{id};
-        try (Cursor cursor =
-                     database.rawQuery("select * from `" + table + "` where id == ? limit 1", args)) {
-            if (cursor.getCount() <= 0) {
-                return null;
-            }
-            markAsCached(table, id);
-            cursor.moveToFirst();
-            return DatabaseUtils.cursorToMap(cursor);
-        }
-    }
-
-    public WritableArray cachedQuery(String table, String query, Object[] args) {
-        WritableArray resultArray = Arguments.createArray();
-        try (Cursor cursor = database.rawQuery(query, args)) {
-            if (cursor.getCount() > 0 && DatabaseUtils.arrayContains(cursor.getColumnNames(), "id")) {
-                int idColumnIndex = cursor.getColumnIndex("id");
-                while (cursor.moveToNext()) {
-                    String id = cursor.getString(idColumnIndex);
-                    if (isCached(table, id)) {
-                        resultArray.pushString(id);
-                    } else {
-                        markAsCached(table, id);
-                        resultArray.pushMap(DatabaseUtils.cursorToMap(cursor));
-                    }
-                }
-            }
-        }
-        return resultArray;
-    }
-
-    public WritableArray queryIds(String query, Object[] args) {
-        WritableArray resultArray = Arguments.createArray();
-        try (Cursor cursor = database.rawQuery(query, args)) {
-            if (cursor.getCount() > 0 && DatabaseUtils.arrayContains(cursor.getColumnNames(), "id")) {
-                while (cursor.moveToNext()) {
-                    int columnIndex = cursor.getColumnIndex("id");
-                    resultArray.pushString(cursor.getString(columnIndex));
-                }
-            }
-        }
-        return resultArray;
-    }
-
-    public WritableArray unsafeQueryRaw(String query, Object[] args) {
-        WritableArray resultArray = Arguments.createArray();
-        try (Cursor cursor = database.rawQuery(query, args)) {
-            if (cursor.getCount() > 0) {
-                while (cursor.moveToNext()) {
-                    resultArray.pushMap(DatabaseUtils.cursorToMap(cursor));
-                }
-            }
-        }
-        return resultArray;
-    }
-
-    public int count(String query, Object[] args) {
-        return database.count(query, args);
-    }
-
-    public String getLocal(String key) {
-        return database.getFromLocalStorage(key);
-    }
-
-    public void batch(ReadableArray operations) {
-        List<Pair<String, String>> newIds = new ArrayList<>();
-        List<Pair<String, String>> removedIds = new ArrayList<>();
-
-        Trace.beginSection("Batch");
-        try {
-            database.transaction(() -> {
-                for (int i = 0; i < operations.size(); i++) {
-                    ReadableArray operation = operations.getArray(i);
-                    int cacheBehavior = operation.getInt(0);
-                    String table = cacheBehavior != 0 ? operation.getString(1) : "";
-                    String sql = operation.getString(2);
-                    ReadableArray argBatches = operation.getArray(3);
-
-                    for (int j = 0; j < argBatches.size(); j++) {
-                        Object[] args = argBatches.getArray(j).toArrayList().toArray();
-                        database.execute(sql, args);
-                        if (cacheBehavior != 0) {
-                            String id = (String) args[0];
-                            if (cacheBehavior == 1) {
-                                newIds.add(Pair.create(table, id));
-                            } else if (cacheBehavior == -1) {
-                                removedIds.add(Pair.create(table, id));
-                            }
-                        }
-                    }
-                }
-            });
-        } finally {
-            Trace.endSection();
-        }
-
-        Trace.beginSection("updateCaches");
-        for (Pair<String, String> it : newIds) {
-            markAsCached(it.first, it.second);
-        }
-        for (Pair<String, String> it : removedIds) {
-            removeFromCache(it.first, it.second);
-        }
-        Trace.endSection();
-    }
-
-    public void provideSyncJson(int id, byte[] syncPullJson) {
-        // Implement if needed
-    }
-
-    public void unsafeResetDatabase(Schema schema) {
-        database.unsafeExecuteStatements(schema.getSql());
-    }
-
-    public boolean find(String table, String id, String[] args) {
-        String[] newArgs = new String[]{id};
-        try (Cursor cursor =
-                     database.rawQuery("select * from `" + table + "` where id == ? limit 1", newArgs)) {
+        try (Cursor cursor = database.rawQuery("select * from `" + table + "` where id == ? limit 1", args)) {
             return cursor.getCount() > 0;
         }
     }
@@ -218,31 +58,68 @@ public class WMDatabaseDriver {
         }
     }
 
-    public String queryIds(String query, String[] args) {
-        try (Cursor cursor = database.rawQuery(query, args)) {
-            StringBuilder builder = new StringBuilder();
-            builder.append("[");
-            if (cursor.moveToFirst()) {
-                do {
-                    if (builder.length() > 1) {
-                        builder.append(",");
+    public WritableArray cachedQuery(String table, String query, ReadableArray args) {
+        String[] stringArgs = new String[args.size()];
+        for (int i = 0; i < args.size(); i++) {
+            stringArgs[i] = String.valueOf(args.getString(i));
+        }
+        try (Cursor cursor = database.rawQuery(query, stringArgs)) {
+            WritableArray resultArray = Arguments.createArray();
+            if (cursor.getCount() > 0 && cursor.getColumnNames()[0].equals("id")) {
+                int idColumnIndex = cursor.getColumnIndex("id");
+                while (cursor.moveToNext()) {
+                    String id = cursor.getString(idColumnIndex);
+                    if (isCached(table, id)) {
+                        resultArray.pushString(id);
+                    } else {
+                        markAsCached(table, id);
+                        resultArray.pushMap(DatabaseUtils.cursorToMap(cursor));
                     }
-                    builder.append("\"").append(cursor.getString(0)).append("\"");
-                } while (cursor.moveToNext());
+                }
             }
-            builder.append("]");
-            return builder.toString();
+            return resultArray;
         }
     }
 
-    public String unsafeQueryRaw(String query, String[] args) {
-        try (Cursor cursor = database.rawQuery(query, args)) {
-            return encodeCursor(cursor);
+    public WritableArray queryIds(String query, ReadableArray args) {
+        String[] stringArgs = new String[args.size()];
+        for (int i = 0; i < args.size(); i++) {
+            stringArgs[i] = String.valueOf(args.getString(i));
+        }
+        try (Cursor cursor = database.rawQuery(query, stringArgs)) {
+            WritableArray resultArray = Arguments.createArray();
+            if (cursor.getCount() > 0 && cursor.getColumnNames()[0].equals("id")) {
+                while (cursor.moveToNext()) {
+                    int columnIndex = cursor.getColumnIndex("id");
+                    resultArray.pushString(cursor.getString(columnIndex));
+                }
+            }
+            return resultArray;
         }
     }
 
-    public int count(String query, String[] args) {
-        try (Cursor cursor = database.rawQuery(query, args)) {
+    public WritableArray unsafeQueryRaw(String query, ReadableArray args) {
+        String[] stringArgs = new String[args.size()];
+        for (int i = 0; i < args.size(); i++) {
+            stringArgs[i] = String.valueOf(args.getString(i));
+        }
+        try (Cursor cursor = database.rawQuery(query, stringArgs)) {
+            WritableArray resultArray = Arguments.createArray();
+            if (cursor.getCount() > 0) {
+                while (cursor.moveToNext()) {
+                    resultArray.pushMap(DatabaseUtils.cursorToMap(cursor));
+                }
+            }
+            return resultArray;
+        }
+    }
+
+    public int count(String query, ReadableArray args) {
+        String[] stringArgs = new String[args.size()];
+        for (int i = 0; i < args.size(); i++) {
+            stringArgs[i] = String.valueOf(args.getString(i));
+        }
+        try (Cursor cursor = database.rawQuery(query, stringArgs)) {
             cursor.moveToFirst();
             return cursor.getInt(0);
         }
@@ -257,12 +134,35 @@ public class WMDatabaseDriver {
         }
     }
 
-    public void batch(String operations) {
-        database.unsafeExecuteStatements(operations);
+    public void batch(ReadableArray operations) {
+        logger.info("Executing batch operations");
+        for (int i = 0; i < operations.size(); i++) {
+            ReadableArray operation = operations.getArray(i);
+            int cacheBehavior = operation.getInt(0);
+            String table = cacheBehavior != 0 ? operation.getString(1) : "";
+            String sql = operation.getString(2);
+            ReadableArray argBatches = operation.getArray(3);
+
+            for (int j = 0; j < argBatches.size(); j++) {
+                Object[] args = argBatches.getArray(j).toArrayList().toArray();
+                database.execute(sql, args);
+                if (cacheBehavior != 0) {
+                    String id = (String) args[0];
+                    if (cacheBehavior == 1) {
+                        markAsCached(table, id);
+                    } else if (cacheBehavior == -1) {
+                        removeFromCache(table, id);
+                    }
+                }
+            }
+        }
+        logger.info("Batch operations completed");
     }
 
-    public void unsafeDestroyEverything() {
-        database.unsafeExecuteStatements("drop table if exists local_storage");
+    public void unsafeResetDatabase(Schema schema) {
+        logger.info("Resetting database");
+        database.unsafeExecuteStatements(schema.getSql());
+        logger.info("Database reset completed");
     }
 
     private String encodeCursor(Cursor cursor) {
@@ -321,7 +221,6 @@ public class WMDatabaseDriver {
     }
 
     private void markAsCached(String table, String id) {
-        // log.info("Mark as cached " + id);
         List<String> cache = cachedRecords.get(table);
         if (cache == null) {
             cache = new ArrayList<>();
@@ -345,48 +244,5 @@ public class WMDatabaseDriver {
 
     public void close() {
         database.close();
-    }
-
-    private void migrate(MigrationSet migrations) {
-        int databaseVersion = database.getUserVersion();
-        if (databaseVersion != migrations.from) {
-            throw new IllegalArgumentException("Incompatible migration set applied. " +
-                    "DB: " + databaseVersion + ", migration: " + migrations.from);
-        }
-        database.transaction(() -> {
-            database.unsafeExecuteStatements(migrations.sql);
-            database.setUserVersion(migrations.to);
-        });
-    }
-
-    private static class SchemaCompatibility {
-        static class Compatible extends SchemaCompatibility {
-        }
-
-        static class NeedsSetup extends SchemaCompatibility {
-        }
-
-        static class NeedsMigration extends SchemaCompatibility {
-            final int fromVersion;
-
-            NeedsMigration(int fromVersion) {
-                this.fromVersion = fromVersion;
-            }
-        }
-    }
-
-    private SchemaCompatibility isCompatible(int schemaVersion) {
-        int databaseVersion = database.getUserVersion();
-        if (databaseVersion == schemaVersion) {
-            return new SchemaCompatibility.Compatible();
-        } else if (databaseVersion == 0) {
-            return new SchemaCompatibility.NeedsSetup();
-        } else if (databaseVersion < schemaVersion) {
-            return new SchemaCompatibility.NeedsMigration(databaseVersion);
-        } else {
-            log.info("com.nozbe.watermelondb.Database has newer version (" + databaseVersion + ") than what the " +
-                    "app supports (" + schemaVersion + "). Will reset database.");
-            return new SchemaCompatibility.NeedsSetup();
-        }
     }
 }
