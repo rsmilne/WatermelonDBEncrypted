49b1086c10bff6b6d98a1d496d0081aa135623a6
rsmilne <rodneysmilne@gmail.com>
2024-12-27 13:31:28 -0300

    fix

--- native/android/src/main/java/com/nozbe/watermelondb/encrypted/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-package com.nozbe.watermelondb.encrypted;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-    public static final String LIBRARY_PACKAGE_NAME = "com.nozbe.watermelondb.encrypted";
-    public static final String BUILD_TYPE = "debug";
-}
