e95d8f7d830b2953ba95d0e6b866dadb79e7b541
rsmilne <rodneysmilne@gmail.com>
2024-12-27 13:57:08 -0300

    fix

--- native/android/src/main/java/com/nozbe/watermelondb/encrypted/Connection.java
+++ native/android/src/main/java/com/nozbe/watermelondb/encrypted/Connection.java
@@ -1,23 +1,40 @@
 package com.nozbe.watermelondb.encrypted;
 
-public class Connection {
-    public static class Connected {
-        public final WMDatabaseDriver driver;
+import java.util.ArrayList;
 
+public abstract class Connection {
+    public static class Connected extends Connection {
+        public final WMDatabaseDriver driver;
+        
         public Connected(WMDatabaseDriver driver) {
             this.driver = driver;
         }
-    }
 
-    public static class Disconnected {
-    }
-
-    public ArrayList<Runnable> getQueue() {
-        if (this instanceof Connected) {
+        @Override
+        public ArrayList<Runnable> getQueue() {
             return new ArrayList<>();
-        } else if (this instanceof Waiting) {
-            return ((Waiting) this).queueInWaiting;
         }
-        return null;
     }
+
+    public static class Waiting extends Connection {
+        public final ArrayList<Runnable> queueInWaiting;
+        
+        public Waiting(ArrayList<Runnable> queueInWaiting) {
+            this.queueInWaiting = queueInWaiting;
+        }
+
+        @Override
+        public ArrayList<Runnable> getQueue() {
+            return queueInWaiting;
+        }
+    }
+
+    public static class Disconnected extends Connection {
+        @Override
+        public ArrayList<Runnable> getQueue() {
+            return new ArrayList<>();
+        }
+    }
+
+    public abstract ArrayList<Runnable> getQueue();
 }
